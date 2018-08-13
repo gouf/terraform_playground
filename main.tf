1b5d38890a55b26cf493e6bb81bdb725663f0204
gouf <innocent.zero@gmail.com>
2018-08-13 17:06:16 +0900

    Move values to variable and replace it

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
+variable "vpc_id" {
+  type    = "string"
+  default = "vpc-0a84e4b7d165f8525"
+}
+
+variable "vpc_subnet_id" {
+  type    = "string"
+  default = "subnet-0e498dea8e195959c"
+}
+
+variable "ec2_open_port" {
+  type    = "string"
+  default = "8080"
+}
+
 provider "aws" {
   region = "ap-northeast-1"
 }
@@ -10,7 +25,7 @@ resource "aws_eip" "example_eip" {
 resource "aws_instance" "example" {
   ami           = "ami-940cdceb"
   instance_type = "t2.micro"
-  subnet_id     = "subnet-0e498dea8e195959c"
+  subnet_id     = "${var.vpc_subnet_id}"
   vpc_security_group_ids = ["${aws_security_group.instance.id}"]
 
   tags {
@@ -20,18 +35,18 @@ resource "aws_instance" "example" {
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
-              nohup busybox httpd -f -p 8080 &
+              nohup busybox httpd -f -p "${var.ec2_open_port}" &
               EOF
 }
 
 resource "aws_security_group" "instance" {
   name ="terraform-example-instance"
 
-  vpc_id = "vpc-0a84e4b7d165f8525"
+  vpc_id = "${var.vpc_id}"
 
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = "${var.ec2_open_port}"
+    to_port     = "${var.ec2_open_port}"
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
