790ac00ac5a4022bde500220bb0bf6b8c7c290f4
gouf <innocent.zero@gmail.com>
2018-08-21 18:56:52 +0900

    defined single AZ VPC, AutoScaling + ELB

--- main.tf
+++ main.tf
@@ -1,57 +1,163 @@
-variable "vpc_id" {
-  type    = "string"
-  default = "vpc-0a84e4b7d165f8525"
-}
-
-variable "vpc_subnet_id" {
-  type    = "string"
-  default = "subnet-0e498dea8e195959c"
-}
-
-variable "ec2_open_port" {
-  type    = "string"
-  default = "8080"
-}
-
 provider "aws" {
   region = "ap-northeast-1"
 }
 
-output "EC2_public_ip" {
-  value = "${aws_instance.example.public_ip}"
+data "aws_availability_zones" "all" {}
+
+#
+# VPC
+#
+resource "aws_vpc" "terraforn_playground" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "terraform-playground-vpc"
+  }
 }
 
-resource "aws_eip" "example_eip" {
-  instance = "${aws_instance.example.id}"
-  vpc      = true
+resource "aws_route_table" "rt_a" {
+  vpc_id = "${aws_vpc.terraforn_playground.id}"
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = "${aws_internet_gateway.igw_a.id}"
+  }
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-940cdceb"
-  instance_type = "t2.micro"
-  subnet_id     = "${var.vpc_subnet_id}"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+resource "aws_route_table_association" "rta_a" {
+  subnet_id      = "${aws_subnet.subnet_a.id}"
+  route_table_id = "${aws_route_table.rt_a.id}"
+}
 
+resource "aws_internet_gateway" "igw_a" {
+  vpc_id = "${aws_vpc.terraforn_playground.id}"
   tags {
-    Name = "terraform-example server"
+    Name = "terraforn_playground"
+  }
+}
+
+resource "aws_subnet" "subnet_a" {
+  vpc_id      = "${aws_vpc.terraforn_playground.id}"
+  cidr_block = "10.0.6.0/24"
+  availability_zone = "ap-northeast-1a"
+
+  tags = {
+    Name = "terraform_subnet_1a"
+  }
+}
+
+
+#
+# Autoscale and ELB
+#
+resource "aws_launch_configuration" "example" {
+  image_id        = "ami-940cdceb"
+  instance_type   = "t2.micro"
+  security_groups = ["${aws_security_group.instance.id}", "${aws_security_group.elb.id}"]
+  key_name        = "ec2"
+  associate_public_ip_address = true
+
+  lifecycle {
+    create_before_destroy = true
   }
 
-  user_data = <<-EOF
-              #!/bin/bash
-              echo "Hello, World" > index.html
-              nohup busybox httpd -f -p "${var.ec2_open_port}" &
-              EOF
+  user_data = <<EOF
+#!/bin/bash
+sudo apt-get update -y
+sudo apt-get install -y nginx
+EOF
 }
 
+resource "aws_autoscaling_group" "example" {
+  launch_configuration = "${aws_launch_configuration.example.id}"
+  availability_zones = ["ap-northeast-1a", "ap-northeast-1d"]
+  vpc_zone_identifier = ["${aws_subnet.subnet_a.id}"]
+
+  load_balancers      = ["${aws_elb.example.name}"]
+  health_check_type   = "ELB"
+
+  min_size = 2
+  max_size = 10
+
+  tag {
+    key                 = "Name"
+    value               = "terraform-example"
+    propagate_at_launch = true
+  }
+}
+
+resource "aws_elb" "example" {
+  name               = "terraform-asg-example"
+  security_groups    = ["${aws_security_group.elb.id}", "${aws_security_group.instance.id}"]
+  subnets            = ["${aws_subnet.subnet_a.id}"]
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  listener {
+    lb_port           = 80
+    lb_protocol       = "http"
+    instance_protocol = "http"
+    instance_port     = 80
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 10
+    timeout             = 3
+    interval            = 60
+    target              = "HTTP:80/"
+  }
+}
+
+#
+# Security groups
+#
 resource "aws_security_group" "instance" {
   name ="terraform-example-instance"
 
-  vpc_id = "${var.vpc_id}"
+  vpc_id = "${aws_vpc.terraforn_playground.id}"
 
   ingress {
-    from_port   = "${var.ec2_open_port}"
-    to_port     = "${var.ec2_open_port}"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_security_group" "elb" {
+  name = "terraform-example-elb"
+  vpc_id = "${aws_vpc.terraforn_playground.id}"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+output "elb_dns_name" {
+  value = "${aws_elb.example.dns_name}"
 }
